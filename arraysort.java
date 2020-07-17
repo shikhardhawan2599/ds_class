8096444ff6d75e16651cecb34414f4f46801d3b4
shikhardhawan2599 <68346808+shikhardhawan2599@users.noreply.github.com>
2020-07-17 10:27:55 -0700

    Add files via upload

---
+++ arraysort.java
@@ -0,0 +1,33 @@
+package datads;
+import java.util.*;
+public class arraysort {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[]= {20,50,30,10,40};
+		
+		//bubble sort
+		int temp=0;
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=1; j<a.length-1;j++)
+			{
+				if(a[j-1]>a[j])
+				{
+					temp=a[j-1];
+					a[j-1]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		for(int i=0;i<a.length;i++)
+			System.out.println(a[i]+" ");
+		
+		
+		//without bubble sort
+		Arrays.sort(a);
+		for(int i=0;i<a.length;i++)
+		System.out.println(a[i]+" ");
+		
+	}
+}
---
+++ optimisedbubblesort.java
@@ -0,0 +1,42 @@
+package datads;
+import java.util.*;
+public class optimisedbubblesort {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner (System.in);
+		int n= input.nextInt();
+
+ int [] a= new int[n];
+ for (int c = 0; c < n; c++)  
+ {
+     a[c] = input.nextInt(); 
+ }
+		boolean swap;
+		for(int i=0;i<n-1;i++)
+		{   
+			swap=false;
+			for(int j=0 ;j<n-1-i;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					int temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+					swap=true;
+				}
+			}
+			
+			if(swap==false)
+			{
+				break;
+			}
+		}
+		
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+
+	}
+
+}
